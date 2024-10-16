15edfc4a580d3b5eb2e14699bd90a58ea86e33dd
super-dinosaur <fanjianqi@tju.edu.cn>
2024-10-16 17:07:21 +0800

    init

--- local_setting.py
+++
@@ -1,7 +0,0 @@
-import os.path as osp
-from icecream import ic
-
-PATH_PROJECT = osp.dirname(osp.abspath(__file__))
-PATH_DATA = osp.join(PATH_PROJECT, 'data')
-PATH_SCRIPT = osp.join(PATH_PROJECT, 'script')
-PATH_LIB = osp.join(PATH_PROJECT, 'lib')
--- pred.bat
+++
@@ -1,3 +0,0 @@
-@REM python local_setting.py
-python script/dump_prompts.py
-python script/pred.py
